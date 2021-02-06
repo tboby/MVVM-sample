3ab7ce28d1cc7c947520de94f44a269d55bd1fbb
Thomas Boby <th.boby@gmail.com>
2021-02-06 13:04:59 +0000

    First working gif showing

---
+++ MVVM-exp/GIF.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace MVVM_exp
+{
+
+    public class GIF
+    {
+        public IReadOnlyCollection<BitmapFrame> Frames { get; set; }
+
+        public GIF(string filename)
+        {
+            LoadImage(filename);
+        }
+        public void LoadImage(string filename)
+        {
+            var image = new GifBitmapDecoder(new Uri(new FileInfo(filename).FullName), BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.Default);
+            Frames = image.Frames;
+        }
+    }
+}
---
+++ MVVM-exp/ImageControlViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+
+namespace MVVM_exp
+{
+    public class ImageControlViewModel : ObservableObject
+    {
+        public BitmapFrame CurrentBitmapFrame
+        {
+            get => _currentBitmapFrame;
+            set => SetProperty(ref _currentBitmapFrame, value);
+        }
+
+        public int CurrentFrameIndex
+        {
+            get => _currentFrameIndex;
+            set => SetProperty(ref _currentFrameIndex, value);
+        }
+        public ICommand ButtonCommand { get; set; }
+
+        private GIF gif;
+        private BitmapFrame _currentBitmapFrame;
+        private int _currentFrameIndex;
+
+        public ImageControlViewModel(GIF gif)
+        {
+            this.gif = gif;
+            CurrentFrameIndex = 0;
+            RedrawImage();
+            ButtonCommand = new RelayCommand(() =>
+            {
+                if (CurrentFrameIndex + 1 == gif.Frames.Count)
+                {
+                    CurrentFrameIndex = 0;
+                }
+                else
+                {
+                    CurrentFrameIndex ++ ;
+                }
+                RedrawImage();
+            });
+
+        }
+
+        public void RedrawImage()
+        {
+            CurrentBitmapFrame = gif.Frames.ElementAt(CurrentFrameIndex);
+        }
+
+    }
+}
